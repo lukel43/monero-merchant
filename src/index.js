bbee3b0837628d1f89f3d64586847320a69edec6
Luke Lacaria <lacaria01@allegheny.edu>
2022-10-06 21:06:44 -0400

    Add failed test wallet creations

--- src/index.js
+++ src/index.js
@@ -1,19 +1,58 @@
 const monerojs = require('monero-javascript');
 
-/* Basic RPC Tests
+// Basic RPC Tests
 const run = async () => {
+  // Connect to daemon RPC
   let daemon = await monerojs.connectToDaemonRpc(
     "http://mmc.chompe.rs:18089"
   );
+  
+  // Connect to wallet RPC
+  /* FAILED
+  let walletRpc = new MoneroWalletRpc.connectToWalletRpc(
+    "http://mmc.chompe.rs:18089"
+  );
+  */
+  
+  // Create keys-only wallet
+  let wallet = await monerojs.createWalletKeys({
+    password: "abc123",
+    networkType: "testnet"
+ });
+  // Grab block height
   let height = await daemon.getHeight();
-  console.log("Block height: ", height);
+  // console.log("Block height: ", height);
+  // Grab fee estimate
   let feeEstimate = await daemon.getFeeEstimate()
-  console.log("Fee Estimate: ", feeEstimate);
-  let txsInPool = await daemon.getTxPool();
-  console.log("Txns in pool: ", txsInPool);
+  // console.log("Fee Estimate: ", feeEstimate);
+  // Grab txns in pool
+  let txsInPool = await daemon.getTxPool(); 
+  // console.log("Txns in pool: " + txsInPool + " \n ========================== \n Txns end");
+  
+  // Create wallet
+  // FAILED TEST
+  // let keysOnlyWallet = await monerojs.createWalletKeys({networkType: "testnet", language: "English"});
+
+  await walletRpc.createWalletFull({
+    path: "test_wallet",
+    password: "password",
+    networkType: "testnet",
+    serverUri: "http://mmc.chompe.rs:18089",
+  });
+
+
+
+  // Output wallet attributes
+  console.log("Mnemonic phrase: " + await walletKeys.getMnemonic());
+  console.log("Address: " + await walletKeys.getAddress(0,0)); // get address of account 0, subaddress 0
+  console.log("Spend key: " + await walletKeys.getPrivateSpendKey());
+  console.log("View key: " + await walletKeys.getPrivateViewKey());
   walletFull.close(true);
+  
 };
-*/
+
+
+/* Failed Tests
 main();
 // Test creating a wallet
 const run = async () => {
@@ -30,6 +69,6 @@ async function main() {
   console.log("Spend key: " + await walletKeys.getPrivateSpendKey());
   console.log("View key: " + await walletKeys.getPrivateViewKey());
 }
-
-//run();
+*/
+run();
 
