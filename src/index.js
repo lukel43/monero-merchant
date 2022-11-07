da7ea4a105d57a701b3f98efdcc3fb4574ebdc8f
Luke Lacaria <lacaria01@allegheny.edu>
2022-11-07 15:04:16 -0500

    Stagenet

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 const monerojs = require('monero-javascript');
 
 const run = async () => {
+  // Daemon working on other nodes but not ours
   let daemon = await monerojs.connectToDaemonRpc(
-    "http://mmc.chompe.rs:18080"
+    uri = "http://mmc.chompe.rs:38081",
+    rejectUnauthorize = false
   );
   let height = await daemon.getHeight();
   console.log(height);
+  
+  /*
+  let walletRpc = await monerojs.connectToWalletRpc(
+    uri = "http://mmc.chompe.rs:18081",
+    networkType = "stagenet"
+  );
+  */
 };
+
+
 
 run();
