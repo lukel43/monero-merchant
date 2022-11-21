75b5e128dc47c91361a0910b352bca792a9902ea
Luke Lacaria <lacaria01@allegheny.edu>
2022-11-21 14:50:09 -0500

    Connect to wallet-rpc

--- src/index.js
+++ src/index.js
@@ -9,14 +9,19 @@ const run = async () => {
   let height = await daemon.getHeight();
   console.log(height);
   
-  /*
+  
   let walletRpc = await monerojs.connectToWalletRpc(
-    uri = "http://mmc.chompe.rs:18081",
-    networkType = "stagenet"
+    "http://mmc.chompe.rs:38081",
+    "test_user",
+    "stagenet"
   );
-  */
+
+  let walletFull = await monerojs.createWalletFull({
+    path: "sample_wallet_full",
+    password: "password123",
+    networkType: "stagenet",
+    serverUri: "http://mmc.chompe.rs:38081",
+  });
 };
-
-
 
 run();
