1d472c3d4a1ec814e4a2b7f3dee3c9b6e6a50c4c
Luke Lacaria <lacaria01@allegheny.edu>
2022-12-05 11:57:40 -0500

    Check balance, transfers, set up tx code

--- src/index.js
+++ src/index.js
@@ -9,20 +9,40 @@ const run = async () => {
   let height = await daemon.getHeight();
   console.log(height);
 
-  let walletRpc = await monerojs.connectToWalletRpc(
-    "http://mmc.chompe.rs:38081",
-  );
+  //let walletRpc = await monerojs.connectToWalletRpc(
+  //  "http://mmc.chompe.rs:38081",
+  //);
 
   //await walletRpc.createWallet({
   //  path: "testwallet",
   //  password: "password123"
   //});
-  let wallet = await monerojs.createWalletFull({
-    path: "./testwallet2",
+  let wallet = await monerojs.openWalletFull({
+    path: "./mining_wallet",
     password: "password123",
     networkType: monerojs.MoneroNetworkType.STAGENET,
     server: new monerojs.MoneroRpcConnection("http://mmc.chompe.rs:38081")
   });
+
+  let balance = await wallet.getBalance();
+  let transfers = await wallet.getTransfers();
+  //console.log("Wallet: ", wallet);
+  console.log("Balance: ", balance);
+  console.log("Transfers: ", transfers);
+
+  /* Test sending funds
+  let tx = await wallet.createTx({
+    accountIndex: 0,  // source account to send funds from
+    address: "565MFUjqb9mUhKnppsE5RW5ssAjfniy2UWBFZX6dcfetYj1eEZrZq41bRyh4axRib5eAN91Efz51mbbxydEpvHhN6WRpqga",
+    amount: "100000000000" // send 0.1 XMR (denominated in atomic units)
+  });
+  
+  // can confirm with the user
+  let fee = tx.getFee();  // "Are you sure you want to send... ?"
+  
+  // relay the transaction
+  let hash = await wallet.relayTx(tx);
+  */
 };
 
 run();
